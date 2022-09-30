f1144844fa0674d5b35b0d4a4423906d54875d4f
joseFastyDev <jose.rangel@fastydev.com>
2022-09-30 17:54:06 -0500

    third commit

--- src/data/menuItems.tsx
+++ src/data/menuItems.tsx
@@ -28,4 +28,24 @@ export const menuItems: MenuItem[] = [
       icon: 'document-text-outline',
       component: 'TextInputScreen'
     },
+    {
+      name: 'Pull to refresh',
+      icon: 'refresh-outline',
+      component: 'PullToRefreshScreen'
+    },
+    {
+      name: 'Section List',
+      icon: 'list-outline',
+      component: 'CustomSectionListScreen'
+    },
+    {
+      name: 'Modal Screen',
+      icon: 'copy-outline',
+      component: 'ModalScreen'
+    },
+    {
+      name: 'InfiniteScroll',
+      icon: 'download-outline',
+      component: 'InfiniteScrollScreen'
+    },
   ]
