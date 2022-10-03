8f36b894b5e4f668e4b27642f0b4b0061266bf33
joseFastyDev <jose.rangel@fastydev.com>
2022-10-03 18:03:54 -0500

    03oct2022

--- App.tsx
+++ App.tsx
@@ -1,14 +1,34 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { Navigator } from './src/navigator/Navigator';
+import { ThemeProvider } from './src/context/themeContext/ThemeContext';
 
+// const customTheme: Theme = {
+//   dark: true,
+//   colors: {
+//     ...DarkTheme.colors
+//     // primary: ,
+//     // background: ,
+//     // card: ,
+//     // text: ,
+//     // border: ,
+//     // notification: ,
+//   }
+// }
 
 const App = () => {
   return (
-    <NavigationContainer>
+    <AppState>
       <Navigator />
-    </NavigationContainer>
+    </AppState>
+  )
+}
+
+const AppState = ({ children }: any) => {
+  return (
+    <ThemeProvider>
+      { children }
+    </ThemeProvider>
   )
 }
 
